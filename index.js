796383469df0aab259b7a0be5f73bcaa40da89a1
aetherstarts <vanzemangkece@gmail.com>
2025-12-25 02:03:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// Folder statis untuk UI
+// Folder statis untuk UI & Assets
 app.use('/', express.static(path.join(__dirname, 'api-page')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
 
-// Middleware untuk format JSON (Menambahkan Creator otomatis)
+// Load Settings untuk creator JSON
 const settingsPath = path.join(__dirname, './src/settings.json');
 const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
 
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
         if (data && typeof data === 'object') {
             const responseData = {
                 status: data.status,
-                creator: settings.apiSettings.creator || "Skyzopedia",
+                creator: settings.apiSettings.creator || "G4NGGAA",
                 ...data
             };
             return originalJson.call(this, responseData);
@@ -42,32 +42,28 @@ app.use((req, res, next) => {
 let totalRoutes = 0;
 const apiFolder = path.join(__dirname, './src/api');
 
-if (fs.existsSync(apiFolder)) {
-    fs.readdirSync(apiFolder).forEach((subfolder) => {
-        const subfolderPath = path.join(apiFolder, subfolder);
-        if (fs.statSync(subfolderPath).isDirectory()) {
-            fs.readdirSync(subfolderPath).forEach((file) => {
-                if (path.extname(file) === '.js') {
-                    const filePath = path.join(subfolderPath, file);
-                    require(filePath)(app);
-                    totalRoutes++;
-                    console.log(chalk.bgCyan.black.bold(` Loaded Route: ${file} `));
-                }
-            });
-        }
-    });
-}
-
-console.log(chalk.bgGreen.black.bold(` Total API Loaded: ${totalRoutes} `));
-
-// Jalankan Server
-app.listen(PORT, () => {
-    console.log(chalk.green.bold(`Server running on http://localhost:${PORT}`));
+fs.readdirSync(apiFolder).forEach((subfolder) => {
+    const subfolderPath = path.join(apiFolder, subfolder);
+    if (fs.statSync(subfolderPath).isDirectory()) {
+        fs.readdirSync(subfolderPath).forEach((file) => {
+            if (path.extname(file) === '.js') {
+                require(path.join(subfolderPath, file))(app);
+                totalRoutes++;
+                console.log(chalk.bgCyan.black(` Loaded Route: ${file} `));
+            }
+        });
+    }
 });
+
+console.log(chalk.bgGreen.black(` Total API Loaded: ${totalRoutes} `));
 
 // Handle 404
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'api-page', '404.html'));
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(chalk.green.bold(`Server running on http://localhost:${PORT}`));
+});
+
+mmodul..export =  ap;
